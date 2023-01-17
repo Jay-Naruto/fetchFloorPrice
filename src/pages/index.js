7589144ec8ac8827be8784674531f93a0aa8cec9
JayNaruto <jaynarutomistry@gmail.com>
2023-01-17 23:50:22 +0530

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,49 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useState } from 'react'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [input,setInput]=useState("");
+  const [fetchImg,setFetchImg]=useState("");
+
+  const [fetchFlprice,setFetchFlprice]=useState("");
+  const [loading,setLoading]=useState(0)
+
+  const fetchFlp=async()=>{
+    setTimeout(async()=>{
+      setLoading(1)
+//       headers:{
+//         "Access-Control-Allow-Origin" : "*", 
+// "Access-Control-Allow-Credentials" : true ,
+// "Content-Type": "application/json",
+//       }
+var requestOptions = {
+  // method: 'GET',
+  // redirect: 'follow',
+  mode:'no-cors',
+
+};
+      const response = await fetch(`https://api-devnet.magiceden.dev/v2/collections/degods/stats`,
+      
+);
+      var data =  response;
+      console.log(data);
+      // setFetchImg(data.collection.banner_image_url);
+      // setFetchFlprice(data.collection.stats.floor_price)
+    },2000)
+
+  }
+  function removeSpace(str)
+  {
+     str = str.replace(/\s/g,'')
+     setInput(str.toLowerCase());
+     console.log(str.toLowerCase())
+ }
+
   return (
     <>
       <Head>
@@ -15,108 +54,43 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
+        <div className='container'>
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+            <span className='note'>Enter slug name of the NFT</span>
+          </div>
+          <div className='btnContainer'>
+          <input className='inputField' name='input' 
+            onKeyDown={event => {
+              if (event.key === 'Enter') {
+                fetchFlp()
+              }
+            }}
+          onChange={(e)=>setInput(e.target.value) } value={input}></input>
+           <button className='fetchBtn' onClick={()=>{fetchFlp();}}>
+            Click
+           </button>
+          </div>
+          <div  className='imgContainer'>
+            {
+              fetchImg ?
+              <>
+                  <img src={fetchImg} alt='' className='fetchImg' />
+            <div>
+             
+              <h1 className='result'>
+              Floor price:&nbsp;{fetchFlprice}
+
+              </h1>
+            </div>
+              </>
+              :
+              <h2 className='note'></h2>
+            }
+        
+
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
       </main>
     </>
   )
